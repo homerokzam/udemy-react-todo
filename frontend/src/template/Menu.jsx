3fa6001821de46bd4e35dfc8a7c4a04232c84d93
Homero Batista Kzam <homero@parkcode.mobi>
2018-08-22 21:31:19 -0300

    Resolvido problema de navegação (router)

--- frontend/src/template/Menu.jsx
+++ frontend/src/template/Menu.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Menu extends Component {
   render() {
@@ -7,14 +8,14 @@ class Menu extends Component {
 <nav className="navbar navbar-inverse bg-inverse">
   <div className="container">
     <div className="navbar-header">
-      <a className="navbar-brand" href="#" >
+      <Link className="navbar-brand" to="#" >
         <i className="fa fa-calendar-check-o"></i>Todo App
-      </a>
+      </Link>
     </div>
     <div id="navbar" className="navbar-collapse">
       <ul className="nav navbar-nav">
-        <li><a href="#/todos">Tarefas</a></li>
-        <li><a href="#/about">Sobre</a></li>
+        <li><Link to="todos">Tarefas</Link></li>
+        <li><Link to="about">Sobre</Link></li>
       </ul>
     </div>
   </div>
